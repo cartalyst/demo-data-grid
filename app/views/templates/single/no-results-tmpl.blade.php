d3ce619f0eba89a016756091f17a030adf93736f
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-02-28 14:52:12 +0000

    update the no results tpl
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- app/views/templates/single/no-results-tmpl.blade.php
+++ app/views/templates/single/no-results-tmpl.blade.php
@@ -1,6 +1,5 @@
 <script type="text/template" data-grid="single" id="no-results-tmpl">
 	<tr>
-		<td colspan="4">{{{ trans('table.no_results') }}}</td>
+		<td colspan="4">No Results</td>
 	</tr>
-
 </script>
