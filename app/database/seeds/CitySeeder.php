a7a1f9e9382cc51fa14f2eca12cfd6854d7970eb
Suhayb El Wardany <me@suw.me>
2014-02-28 04:09:42 +0200

    seeder start date jan 13
    Signed-off-by: Suhayb El Wardany <me@suw.me>

--- app/database/seeds/CitySeeder.php
+++ app/database/seeds/CitySeeder.php
@@ -13,7 +13,7 @@ class CitySeeder extends Seeder {
 
 		foreach ($lines as $line)
 		{
-			$int = rand(320528968,1361994568);
+			$int = rand(1356998400, time());
 
 			$city = new City(array(
 				'country'                   => $line[0],
