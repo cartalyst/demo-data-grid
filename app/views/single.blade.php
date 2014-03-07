851afd3c49412b77a3c86ab1d16f55724f53f583
Suhayb El Wardany <me@suw.me>
2014-03-07 03:42:05 +0200

    update filter labels
    Signed-off-by: Suhayb El Wardany <me@suw.me>

--- app/views/single.blade.php
+++ app/views/single.blade.php
@@ -170,13 +170,13 @@ $(function() {
 			</button>
 
 			<ul class="dropdown-menu" role="menu">
-				<li><a href="#" data-filter="country:us" data-label="country:Country" data-grid="single">United States</a></li>
-				<li><a href="#" data-filter="country:ca" data-label="country:Country" data-grid="single">Canada</a></li>
-				<li><a href="#" data-filter="population:>:10000" data-grid="single">Populations > 10000</a></li>
-				<li><a href="#" data-filter="population:=:5000" data-grid="single">Populations = 5000</a></li>
+				<li><a href="#" data-filter="country:us" data-label="country:Country:United States" data-grid="single">United States</a></li>
+				<li><a href="#" data-filter="country:ca" data-label="country:Country:Canada" data-grid="single">Canada</a></li>
+				<li><a href="#" data-filter="population:>:10000" data-label="population:Population >:10000" data-grid="single">Populations > 10000</a></li>
+				<li><a href="#" data-filter="population:=:5000" data-label="population:Populations is:5000" data-grid="single">Populations = 5000</a></li>
 				<li><a href="#" data-filter="population:>:5000" data-grid="single">Populations > 5000</a></li>
 				<li><a href="#" data-filter="population:<:5000" data-grid="single">Populations < 5000</a></li>
-				<li><a href="#" data-filter="country:us, subdivision:washington, population:<:5000" data-label="country:Country, subdivision:Subdiv, population:Population" data-grid="single">Washington, United States < 5000</a></li>
+				<li><a href="#" data-filter="country:us, subdivision:washington, population:<:5000" data-label="country:Country:United States, subdivision:Subdivision:Washington, population:Population:5000" data-grid="single">Washington, United States < 5000</a></li>
 			</ul>
 
 		</div>
