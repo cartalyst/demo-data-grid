a3518664e48b2608e92b873d3765b4bcad709bc8
drsii <dan.syme@cartalyst.com>
2014-05-19 10:34:44 -0700

    rock it suhayb.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -13,4 +13,97 @@
 	</p>
 </div>
 
+<div class="row">
+	<div class="col-lg-6">
+		<h2>getting started</h2>
+		<p>describe why data-grid is awesome in that it's both a data handler on the backend which comes with one bad ass jquery library.</p>
+	</div>
+	<div class="col-lg-6">
+
+		<pre><code>
+			show the example instantiation code.
+		</code></pre>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-6">
+		<h3>Requirements</h3>
+		<ul>
+			<li>requirement 1</li>
+			<li>requirement 2</li>
+			<li>requirement 3</li>
+		</ul>
+
+	</div>
+	<div class="col-lg-6">
+		<pre><code>
+			composer json & script includes
+		</code></pre>
+
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-6">
+		<h3>Step 1</h3>
+		<p>shortly describe how to pass a data-object to data-grid</p>
+
+	</div>
+	<div class="col-lg-6">
+		<pre><code>
+			code yo
+		</code></pre>
+
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-6">
+		<h3>Step 2</h3>
+		<p>describe the view, scripts, options.</p>
+
+		<dl class="dl-horizontal">
+			<dt>option</dt>
+			<dd>option description</dd>
+			<dt>option</dt>
+			<dd>option description</dd>
+			<dt>option</dt>
+			<dd>option description</dd>
+			<dt>option</dt>
+			<dd>option description</dd>
+		</dl>
+
+	</div>
+	<div class="col-lg-6">
+		<pre><code>
+			code yo
+		</code></pre>
+
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-6">
+		<h3>Step 3</h3>
+		<p>describe templates, data-attributes</p>
+
+		<dl class="dl-horizontal">
+			<dt>data-attribute</dt>
+			<dd>attribute description</dd>
+			<dt>data-attribute</dt>
+			<dd>attribute description</dd>
+			<dt>data-attribute</dt>
+			<dd>attribute description</dd>
+			<dt>data-attribute</dt>
+			<dd>attribute description</dd>
+		</dl>
+
+	</div>
+	<div class="col-lg-6">
+		<pre><code>
+			basic 3 column table example.
+		</code></pre>
+	</div>
+</div>
+
 @stop
