5db9a1e51d3b30eb1d54dca564ffca03e80040e8
Suhayb El Wardany <me@suw.me>
2014-05-20 03:16:58 +0200

    add code and attribute definitions
    Signed-off-by: Suhayb El Wardany <me@suw.me>

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -19,9 +19,8 @@
 		<p>describe why data-grid is awesome in that it's both a data handler on the backend which comes with one bad ass jquery library.</p>
 	</div>
 	<div class="col-lg-6">
-
 		<pre><code>
-			show the example instantiation code.
+		$.datagrid('main', '.table', '.pagination', '.filters');
 		</code></pre>
 	</div>
 </div>
@@ -33,11 +32,10 @@
 			<li>requirement 2</li>
 			<li>requirement 3</li>
 		</ul>
-
 	</div>
 	<div class="col-lg-6">
 		<pre><code>
-			composer json & script includes
+		composer json & script includes
 		</code></pre>
 
 	</div>
@@ -51,7 +49,15 @@
 	</div>
 	<div class="col-lg-6">
 		<pre><code>
-			code yo
+		Route::get('source', function()
+		{
+		    $data = new City;
+
+		    return DataGrid::make($post, [
+		        'country',
+		        'city',
+		    ]);
+		});
 		</code></pre>
 
 	</div>
@@ -88,20 +94,40 @@
 		<p>describe templates, data-attributes</p>
 
 		<dl class="dl-horizontal">
-			<dt>data-attribute</dt>
-			<dd>attribute description</dd>
-			<dt>data-attribute</dt>
-			<dd>attribute description</dd>
-			<dt>data-attribute</dt>
-			<dd>attribute description</dd>
-			<dt>data-attribute</dt>
-			<dd>attribute description</dd>
+			<dt>data-grid</dt>
+			<dd>required to identify ownership to the data grid object</dd>
+			<dt>data-filter</dt>
+			<dd>apply filters</dd>
+			<dt>data-label</dt>
+			<dd>fix labels that appear on the filters template</dd>
+			<dt>data-sort</dt>
+			<dd>apply sorts</dd>
+			<dt>data-reset</dt>
+			<dd>resets the grid</dd>
+			<dt>data-template</dt>
+			<dd>required on the underscore templates to identify its role</dd>
 		</dl>
 
 	</div>
 	<div class="col-lg-6">
 		<pre><code>
-			basic 3 column table example.
+
+		<div data-grid="main" class="filters"></div>
+
+		<table data-grid="main" class="table" data-source="{{ URL::to('source') }}">
+
+		    <thead>
+		        <tr>
+		            <th data-sort="country">Country</th>
+		            <th data-sort="city">City</th>
+		        </tr>
+		    </thead>
+		    <tbody></tbody>
+
+		</table>
+
+		<ul class="pagination"></ul>
+
 		</code></pre>
 	</div>
 </div>
